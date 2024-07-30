e7b43e3492ba50e4624f7a9c706874f1b34d2857
Oleksii Shtanko <shtankopro@gmail.com>
2024-07-30 23:55:24 +0200

    WIP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,32 +1,29 @@
+val projectJvmTarget = 17
+
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.spotless)
 }
 
 android {
-    compileSdk = libs.versions.compile.sdk.version.get().toInt()
+    namespace = "dev.shtanko.skeleton"
+    compileSdk = 35
 
     defaultConfig {
-        minSdk = libs.versions.min.sdk.version.get().toInt()
-        targetSdk = libs.versions.target.sdk.version.get().toInt()
+        applicationId = "dev.shtanko.skeleton"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
 
-        applicationId = AppCoordinates.APP_ID
-        versionCode = AppCoordinates.APP_VERSION_CODE
-        versionName = AppCoordinates.APP_VERSION_NAME
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    buildFeatures {
-        viewBinding = true
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -34,38 +31,106 @@ android {
             )
         }
     }
-
-    lint {
-        warningsAsErrors = true
-        abortOnError = true
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions {
+        jvmTarget = projectJvmTarget.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+}
 
-    // Use this block to configure different flavors
-//    flavorDimensions("version")
-//    productFlavors {
-//        create("full") {
-//            dimension = "version"
-//            applicationIdSuffix = ".full"
-//        }
-//        create("demo") {
-//            dimension = "version"
-//            applicationIdSuffix = ".demo"
-//        }
-//    }
+spotless {
+    kotlin {
+        target(
+            fileTree(
+                mapOf(
+                    "dir" to ".",
+                    "include" to listOf("**/*.kt"),
+                    "exclude" to listOf("**/build/**", "**/spotless/*.kt"),
+                ),
+            ),
+        )
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+        val delimiter = "^(package|object|import|interface|internal|@file|//startfile)"
+        val licenseHeaderFile = rootProject.file("spotless/copyright.kt")
+        licenseHeaderFile(licenseHeaderFile, delimiter)
+    }
+}
+
+subprojects {
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+}
+
+tasks {
+    withType<io.gitlab.arturbosch.detekt.Detekt> {
+        description = "Runs over whole code base without the starting overhead for each module."
+        parallel = true
+        baseline.set(file("$rootDir/config/detekt/detekt-baseline.xml"))
+        config.from(file("$rootDir/config/detekt/detekt.yml"))
+        jvmTarget = "$projectJvmTarget"
+
+        setSource(files("src/main/kotlin", "src/test/kotlin"))
+        setOf(
+            "**/*.kt",
+            "**/*.kts",
+            ".*/resources/.*",
+            ".*/build/.*",
+            "/versions.gradle.kts",
+        ).forEach {
+            include(it)
+        }
+
+        reports {
+            reports.apply {
+                listOf(xml, html, txt, md).map { it.required }.forEach {
+                    it.set(true)
+                }
+            }
+        }
+    }
 }
 
 dependencies {
-    implementation(projects.libraryAndroid)
-    implementation(projects.libraryKotlin)
+    libs.apply {
+        androidx.apply {
+            implementation(core.ktx)
+            implementation(lifecycle.runtime.ktx)
+            implementation(activity.compose)
+            implementation(platform(compose.bom))
+            implementation(ui)
+            implementation(ui.graphics)
+            implementation(ui.tooling.preview)
+            implementation(material3)
 
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraint.layout)
-    implementation(libs.androidx.core.ktx)
+            androidTestImplementation(junit)
+            androidTestImplementation(espresso.core)
+            androidTestImplementation(platform(compose.bom))
+            androidTestImplementation(ui.test.junit4)
 
-    testImplementation(libs.junit)
+            debugImplementation(ui.tooling)
+            debugImplementation(ui.test.manifest)
+        }
 
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit.ktx)
-    androidTestImplementation(libs.androidx.test.rules)
-    androidTestImplementation(libs.espresso.core)
+        kotlin.apply {
+            implementation(stdlib)
+            implementation(reflect)
+            implementation(coroutines)
+            implementation(coroutines.debug)
+        }
+
+        testImplementation(junit)
+        testImplementation(assertj)
+        testImplementation(mockito)
+        testImplementation(mockito.kotlin)
+        testImplementation(kotest)
+        testImplementation(kotest.assertions)
+        testImplementation(kotest.property)
+        testImplementation(okhttp.mockwebserver)
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,23 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = ("kotlin-base-skeleton")
-
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-include(
-    "app",
-    "library-android",
-    "library-kotlin"
-)
+rootProject.name = "android-base-skeleton"
+include(":app")
